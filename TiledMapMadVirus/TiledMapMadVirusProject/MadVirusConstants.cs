5e9c5fbdca3a2fc1655f1736042b381d7209cb76
Vinh Nhon <thevinh92@gmail.com>
2015-03-10 07:05:52 -0700

    Draw map from a array with just 1 virus sprite

---
+++ TiledMapMadVirus/TiledMapMadVirusProject/MadVirusConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiledMapMadVirusProject
+{
+    
+    class MadVirusConstants
+    {
+        public const float VIRUS_SPRITE_WIDTH = 84.0f;
+        public const float VIRUS_SPRITE_HEIGHT = 74.0f;
+
+
+    }
+}
--- TiledMapMadVirus/TiledMapMadVirusProject/MyScene.cs
+++ TiledMapMadVirus/TiledMapMadVirusProject/MyScene.cs
@@ -14,6 +14,8 @@ using WaveEngine.Framework.Services;
 using WaveEngine.Components.Animation;
 using WaveEngine.ImageEffects;
 using WaveEngine.TiledMap;
+
+using TiledMapMadVirusProject;
 #endregion
 
 namespace TiledMapMadVirusProject
@@ -21,11 +23,17 @@ namespace TiledMapMadVirusProject
     public class MyScene : Scene
     {
         private TiledMap tileMap;
+        private int[,] virusIndexArray;
         protected override void CreateScene()
         {
             //Insert your scene definition here.
             this.CreateCamera();
-            this.CreateTileMap();
+            //this.CreateTileMap();
+            virusIndexArray = this.generateRandomMap(6, 9);
+            this.print2DArray(virusIndexArray);
+            // after generate an array of virus index
+            // create the sprite of virus and add to entity manager
+            this.CreateVirusMap(virusIndexArray);
         }
 
         private void CreateCamera()
@@ -37,7 +45,7 @@ namespace TiledMapMadVirusProject
             };
 
             var camera2DComponent = camera.Entity.FindComponent<Camera2D>();
-            camera2DComponent.Zoom = Vector2.One / 2.5f;
+//            camera2DComponent.Zoom = Vector2.One / 2.5f;
 
             #region Lens Effects
             ////if (WaveServices.Platform.PlatformType == PlatformType.Windows ||
@@ -68,6 +76,56 @@ namespace TiledMapMadVirusProject
             this.EntityManager.Add(map);
         }
 
+        private void CreateVirusMap(int[,] arr)
+        {
+
+            System.Console.WriteLine("row count: {0}", arr.GetLength(0));
+            System.Console.WriteLine("Column count: {0}", arr.GetLength(1));
+            Transform2D sampleTrans = new Transform2D();// by default: X = 0, Y = 0, top-left conner
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                 //for(int j = 0; j < arr.GetLength(1); j++)
+                for (int j = 0; j < arr.GetLength(1); j++)
+                 {
+                        Entity virus = new Entity("virus" + i.ToString() + j.ToString())
+                         .AddComponent(new Transform2D()
+                         {
+                             X = j * MadVirusConstants.VIRUS_SPRITE_WIDTH * 3 / 4, // Don't ask why, I just found the formula
+                             Y = i * MadVirusConstants.VIRUS_SPRITE_HEIGHT + (j % 2) * 74 / 2 //  Don't ask why, I just found the formula
+                         })
+                        .AddComponent(new Sprite("Content/virut_blue.wpk"))
+                        .AddComponent(new SpriteRenderer(DefaultLayers.Alpha));
+                                                EntityManager.Add(virus);
+                }
+        }
+    }
+
+        private int[,] generateRandomMap(int r, int q)
+        {
+            int[,] arrayMap = new int[r,q];
+            System.Random rnd = new System.Random();
+            for (int i = 0; i < r; i++ )
+            {
+                for (int j = 0; j< q; j++)
+                {
+                    
+                    arrayMap[i, j] = rnd.Next(8);
+                }
+            }
+            return arrayMap;
+        }
+
+        private void print2DArray(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    System.Random rnd = new System.Random();
+                    System.Console.WriteLine("Element({0},{1})={2}", i, j, arr[i, j]);
+                }
+            }
+        }
         protected override void Start()
         {
             base.Start();
